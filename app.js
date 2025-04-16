5b8d8241f34e978fdfaeaa09082f9f5f0cbfd4fb
Kashansaleem192 <kashansaleem192@gmail.com>
2025-04-16 14:45:08 +0500

    JS Assignment 8

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 var display = document.getElementById('display');
 
-
 function typeValue(value) {
   display.value += value;
 }
 
-
 function clearDisplay() {
   display.value = '';
 }
-
 
 function deleteLast() {
   display.value = display.value.slice(0, -1);
@@ -24,10 +21,13 @@ function calculateResult() {
   for (let i = 0; i < input.length; i++) {
     const char = input.charAt(i);
 
-    if (char >= '0' && char <= '9' || char === '.') {
+    if ((char >= '0' && char <= '9') || char === '.') {
       currentNumber += char;
     }
-    
+
+    if (char === '%') {
+      display.value= (parseFloat(currentNumber) / 100).toString();
+    }
 
     if (char === '+' || char === '-' || char === '*' || char === '/') {
       if (operator === '+') {
@@ -40,35 +40,33 @@ function calculateResult() {
         result /= parseFloat(currentNumber);
       }
 
-      
       currentNumber = '';
       operator = char;
     }
-
-   
-    if (char === '%') {
-      result = result * 0.01;
-    }
   }
 
- 
-  if (operator === '+') {
-    result += parseFloat(currentNumber);
-  } else if (operator === '-') {
-    result -= parseFloat(currentNumber);
-  } else if (operator === '*') {
-    result *= parseFloat(currentNumber);
-  } else if (operator === '/') {
-    result /= parseFloat(currentNumber);
+  if (currentNumber !== '') {
+    if (operator === '+') {
+      result += parseFloat(currentNumber);
+    } else if (operator === '-') {
+      result -= parseFloat(currentNumber);
+    } else if (operator === '*') {
+      result *= parseFloat(currentNumber);
+    } else if (operator === '/') {
+      result /= parseFloat(currentNumber);
+    }
   }
 
   display.value = result;
 }
 
 function squareValue() {
-  let currentInput = display.value;
-  if (currentInput) {
-    let number = parseFloat(currentInput);
-    display.value = Math.pow(number, 2); 
-  }
+  display.value = display.value * display.value;
+}
+
+function percentValue(){
+  let result = parseFloat(display.value);
+
+
+  display.value = (result / 100).toString();
 }
